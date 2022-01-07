a6f73e320ea281a7e2b4553c9f5c535b4d544b3d
kiki-doyouloveme <2858785543@qq.com>
2022-01-07 17:56:15 +0800

    beauty, mood

--- pages/equip/function/beauty.vue
+++ pages/equip/function/beauty.vue
@@ -3,7 +3,11 @@
 		<camera device-position="back" flash="off" style="width: 100%;height: 400upx;">
 
 		</camera>
-		
+<!-- 		<view style="width: 100%;text-align: center;margin-top: 30rpx;">
+			<u--image :src=origin width="100%" style="margin-left: 5%;margin-right: 5%;margin-top: 50rpx;" mode="widthFix">
+				<view slot="error" style="font-size: 24rpx;">加载失败</view>
+			</u--image>
+		</view> -->
 		<view style="width: 100%;text-align: center;margin-top: 50rpx;">
 			<text style="font-size: 40rpx;">选择口红色号</text>
 			<u-radio-group v-model="radiovalue1" placement="column" borderBottom iconPlacement="right" @change="groupChange" size="30" style="width: 60%;margin-left: 20%;margin-top: 50rpx;">
@@ -12,12 +16,19 @@
 				</u-radio>
 			</u-radio-group>
 		</view>
-		<u-button style="width: 60%;border-radius: 50px;margin-top: 40rpx;" text="生成结果" color="#3fd1ad" @click="Back">
+		<u-button style="width: 60%;border-radius: 50px;margin-top: 40rpx;" text="生成结果" color="#3fd1ad" @click="faceBeauty()">
 		</u-button>
+		<view v-show=showResult style="width: 100%;text-align: center;margin-top: 30rpx;">
+			<text style="font-size: 40rpx;font-weight: 600;">结果展示</text>
+			<u--image :src=result width="100%" style="margin-left: 5%;margin-right: 5%;margin-top: 50rpx;" mode="widthFix">
+				<view slot="error" style="font-size: 24rpx;">加载失败</view>
+			</u--image>
+		</view>
 	</view>
 </template>
 
 <script>
+	import {beauty} from "../../../api/function/function.js"
 	export default {
 		data() {
 			return {
@@ -43,11 +54,12 @@
 				],
 				// u-radio-group的v-model绑定的值如果设置为某个radio的name，就会被默认选中
 				radiovalue1: '正红',
+				showResult: false,
+				origin: "https://cdn.uviewui.com/uview/album/1.jpg",
+				result: "https://cdn.uviewui.com/uview/album/1.jpg"
 			};
 		},
-
 		onLoad() {
-
 		},
 		methods: {
 			groupChange(n) {
@@ -55,7 +67,49 @@
 			},
 			radioChange(n) {
 				console.log('radioChange', n);
+			},
+			image2Base64(url, callback) {
+				var Img = new Image(), dataURL = '';
+				Img.src = url + '?v=' + Math.random();
+				Img.setAttribute('crossOrigin', 'Anonymous');
+				Img.onload = function() {
+					var canvas = document.createElement('canvas'),
+				    width = Img.width,
+				    height = Img.height;
+				    canvas.width = width;
+				    canvas.height = height;
+				    canvas.getContext('2d').drawImage(Img, 0, 0, width, height);
+				    dataURL = canvas.toDataURL('image/jpg');
+				    return callback ? callback(dataURL) : null;
+				}
+			},
+			faceBeauty() {
+				let radiocolor;
+				let faceBase64;
+				let list = this.radiolist1;
+				for (var i = 0; i < 4; i++) {
+					if (list[i].name === this.radiovalue1) {
+						radiocolor = list[i].color;
+					}
+				}
+				console.log(radiocolor)
+				this.image2Base64(this.origin, dataURL => {
+					//console.log(dataURL);
+					faceBase64 = dataURL.split(",")[1];
+					beauty(faceBase64, radiocolor).then(res => {
+						if (res.data.code === 200) {
+							this.result = "data:image/jpg;base64," + btoa(new Uint8Array(res.data).reduce((res, byte) => res + String.fromCharCode(byte), ''));
+							showResult = true;
+						} else {
+							uni.showModal({
+								content: res.data.msg,
+								showCancel: false
+							});
+						}						   
+					}).catch(failResponse => {});
+				});
 			}
+			
 		}
 	}
 </script>
--- pages/equip/function/mood.vue
+++ pages/equip/function/mood.vue
@@ -3,11 +3,11 @@
 		<camera device-position="back" flash="off" style="width: 100%;height: 400upx;">
 
 		</camera>
-		<u-button style="width: 60%;border-radius: 50px;margin-top: 40rpx;" text="生成结果" color="#3fd1ad" @click="Back">
+		<u-button style="width: 60%;border-radius: 50px;margin-top: 40rpx;" text="生成结果" color="#3fd1ad" @click="emotion()">
 		</u-button>
-		<view style="width: 100%;text-align: center;margin-top: 30rpx;">
+		<view v-show=showResult style="width: 100%;text-align: center;margin-top: 30rpx;">
 			<text style="font-size: 40rpx;font-weight: 600;">结果分析</text>
-			<u--image src="https://cdn.uviewui.com/uview/album/1.jpg" width="100%" style="margin-left: 5%;margin-right: 5%;margin-top: 50rpx;" mode="widthFix">
+			<u--image :src=result width="100%" style="margin-left: 5%;margin-right: 5%;margin-top: 50rpx;" mode="widthFix">
 				<view slot="error" style="font-size: 24rpx;">加载失败</view>
 			</u--image>
 		</view>
@@ -17,11 +17,14 @@
 </template>
 
 <script>
+	import {getEmotion} from "../../../api/function/function.js"
 	export default {
 		name: "mood",
 		data() {
 			return {
-				src: ""
+				src: "https://cdn.uviewui.com/uview/album/1.jpg",
+				showResult: true,
+				result: "https://cdn.uviewui.com/uview/album/1.jpg"
 			}
 		},
 		onLoad() {
@@ -39,6 +42,40 @@
 			},
 			error(e) {
 				console.log(e.detail);
+			},
+			image2Base64(url, callback) {
+				var Img = new Image(), dataURL = '';
+				Img.src = url + '?v=' + Math.random();
+				Img.setAttribute('crossOrigin', 'Anonymous');
+				Img.onload = function() {
+					var canvas = document.createElement('canvas'),
+				    width = Img.width,
+				    height = Img.height;
+				    canvas.width = width;
+				    canvas.height = height;
+				    canvas.getContext('2d').drawImage(Img, 0, 0, width, height);
+				    dataURL = canvas.toDataURL('image/jpg');
+				    return callback ? callback(dataURL) : null;
+				}
+			},
+			emotion () {
+				let faceBase64;
+				this.image2Base64(this.src, dataURL => {
+					
+					faceBase64 = dataURL.split(",")[1];
+					console.log(faceBase64);
+					getEmotion(faceBase64).then(res => {
+						if (res.data.code === 200) {
+							this.result = "data:image/jpg;base64," + btoa(new Uint8Array(res.data).reduce((res, byte) => res + String.fromCharCode(byte), ''));
+							showResult = true;
+						} else {
+							uni.showModal({
+								content: res.data.msg,
+								showCancel: false
+							});
+						}						   
+					}).catch(failResponse => {});
+				});
 			}
 		}
 	}
