cef59917eed3c64c61e2c64cce43d9978783e72c
kiki-doyouloveme <2858785543@qq.com>
2022-01-09 01:22:55 +0800

    music

---
+++ utils/mood.js
@@ -0,0 +1,235 @@
+export const recommendList = [
+	{
+		moodType: '惊讶',
+		musicList: [{
+			id: '1',
+			name: '惊讶',
+			singer: '贝多芬',
+			src: 'http://music.163.com/song/media/outer/url?id=1387574419.mp3',
+		},{
+			id: '2',
+			name: '月光の雲海',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/4a09ac1c230b14911b945449dd0302a1/KGTX/CLTX001/3767cd187b64b8dcf9841915b6f0256c.mp3',
+		},{
+			id: '3',
+			name: 'Summer ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071729/fd150154ea349ee2950438c9f2a183a4/KGTX/CLTX001/654ca7d7d4abab8a85cd23523a5e693d.mp3',
+		},{
+			id: '4',
+			name: 'あの夏へ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/60a15126d200ceab629ebf1dcb6adaef/KGTX/CLTX001/100e772a8a7a988993f228332a43a285.mp3',
+		}, {
+			id: '5',
+			name: '千百度',
+			singer: '许嵩',
+			src: 'https://sharefs.ali.kugou.com/202201071705/43a469e37a93e6959da8c3d536058b2c/KGTX/CLTX001/fe75c71646819dc6508922dee8eb8ad6.mp3',
+		}, {
+			id: '6',
+			name: '成都',
+			singer: '赵磊',
+			src: 'https://sharefs.ali.kugou.com/202201071709/9c0265f2869edcdfefff731026ffcd85/KGTX/CLTX001/a06b033b356bfc974c5245d0195086a5.mp3'
+		}]
+	},
+	{
+		moodType: '生气',
+		musicList: [{
+			id: '1',
+			name: '生气',
+			singer: '生气',
+			src: 'http://music.163.com/song/media/outer/url?id=1387574419.mp3',
+		},{
+			id: '2',
+			name: '月光の雲海',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/4a09ac1c230b14911b945449dd0302a1/KGTX/CLTX001/3767cd187b64b8dcf9841915b6f0256c.mp3',
+		},{
+			id: '3',
+			name: 'Summer ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071729/fd150154ea349ee2950438c9f2a183a4/KGTX/CLTX001/654ca7d7d4abab8a85cd23523a5e693d.mp3',
+		},{
+			id: '4',
+			name: 'あの夏へ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/60a15126d200ceab629ebf1dcb6adaef/KGTX/CLTX001/100e772a8a7a988993f228332a43a285.mp3',
+		}, {
+			id: '5',
+			name: '千百度',
+			singer: '许嵩',
+			src: 'https://sharefs.ali.kugou.com/202201071705/43a469e37a93e6959da8c3d536058b2c/KGTX/CLTX001/fe75c71646819dc6508922dee8eb8ad6.mp3',
+		}, {
+			id: '6',
+			name: '成都',
+			singer: '赵磊',
+			src: 'https://sharefs.ali.kugou.com/202201071709/9c0265f2869edcdfefff731026ffcd85/KGTX/CLTX001/a06b033b356bfc974c5245d0195086a5.mp3'
+		}]
+	},	{
+		moodType: '喜悦',
+		musicList: [{
+			id: '1',
+			name: '喜悦',
+			singer: '贝多芬',
+			src: 'http://music.163.com/song/media/outer/url?id=1387574419.mp3',
+		},{
+			id: '2',
+			name: '月光の雲海',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/4a09ac1c230b14911b945449dd0302a1/KGTX/CLTX001/3767cd187b64b8dcf9841915b6f0256c.mp3',
+		},{
+			id: '3',
+			name: 'Summer ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071729/fd150154ea349ee2950438c9f2a183a4/KGTX/CLTX001/654ca7d7d4abab8a85cd23523a5e693d.mp3',
+		},{
+			id: '4',
+			name: 'あの夏へ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/60a15126d200ceab629ebf1dcb6adaef/KGTX/CLTX001/100e772a8a7a988993f228332a43a285.mp3',
+		}, {
+			id: '5',
+			name: '千百度',
+			singer: '许嵩',
+			src: 'https://sharefs.ali.kugou.com/202201071705/43a469e37a93e6959da8c3d536058b2c/KGTX/CLTX001/fe75c71646819dc6508922dee8eb8ad6.mp3',
+		}, {
+			id: '6',
+			name: '成都',
+			singer: '赵磊',
+			src: 'https://sharefs.ali.kugou.com/202201071709/9c0265f2869edcdfefff731026ffcd85/KGTX/CLTX001/a06b033b356bfc974c5245d0195086a5.mp3'
+		}]
+	},	{
+		moodType: '中性',
+		musicList: [{
+			id: '1',
+			name: '中性',
+			singer: '贝多芬',
+			src: 'http://music.163.com/song/media/outer/url?id=1387574419.mp3',
+		},{
+			id: '2',
+			name: '月光の雲海',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/4a09ac1c230b14911b945449dd0302a1/KGTX/CLTX001/3767cd187b64b8dcf9841915b6f0256c.mp3',
+		},{
+			id: '3',
+			name: 'Summer ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071729/fd150154ea349ee2950438c9f2a183a4/KGTX/CLTX001/654ca7d7d4abab8a85cd23523a5e693d.mp3',
+		},{
+			id: '4',
+			name: 'あの夏へ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/60a15126d200ceab629ebf1dcb6adaef/KGTX/CLTX001/100e772a8a7a988993f228332a43a285.mp3',
+		}, {
+			id: '5',
+			name: '千百度',
+			singer: '许嵩',
+			src: 'https://sharefs.ali.kugou.com/202201071705/43a469e37a93e6959da8c3d536058b2c/KGTX/CLTX001/fe75c71646819dc6508922dee8eb8ad6.mp3',
+		}, {
+			id: '6',
+			name: '成都',
+			singer: '赵磊',
+			src: 'https://sharefs.ali.kugou.com/202201071709/9c0265f2869edcdfefff731026ffcd85/KGTX/CLTX001/a06b033b356bfc974c5245d0195086a5.mp3'
+		}]
+	},	{
+		moodType: '伤心',
+		musicList: [{
+			id: '1',
+			name: '伤心',
+			singer: '贝多芬',
+			src: 'http://music.163.com/song/media/outer/url?id=1387574419.mp3',
+		},{
+			id: '2',
+			name: '月光の雲海',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/4a09ac1c230b14911b945449dd0302a1/KGTX/CLTX001/3767cd187b64b8dcf9841915b6f0256c.mp3',
+		},{
+			id: '3',
+			name: 'Summer ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071729/fd150154ea349ee2950438c9f2a183a4/KGTX/CLTX001/654ca7d7d4abab8a85cd23523a5e693d.mp3',
+		},{
+			id: '4',
+			name: 'あの夏へ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/60a15126d200ceab629ebf1dcb6adaef/KGTX/CLTX001/100e772a8a7a988993f228332a43a285.mp3',
+		}, {
+			id: '5',
+			name: '千百度',
+			singer: '许嵩',
+			src: 'https://sharefs.ali.kugou.com/202201071705/43a469e37a93e6959da8c3d536058b2c/KGTX/CLTX001/fe75c71646819dc6508922dee8eb8ad6.mp3',
+		}, {
+			id: '6',
+			name: '成都',
+			singer: '赵磊',
+			src: 'https://sharefs.ali.kugou.com/202201071709/9c0265f2869edcdfefff731026ffcd85/KGTX/CLTX001/a06b033b356bfc974c5245d0195086a5.mp3'
+		}]
+	},	{
+		moodType: '厌恶',
+		musicList: [{
+			id: '1',
+			name: '厌恶',
+			singer: '贝多芬',
+			src: 'http://music.163.com/song/media/outer/url?id=1387574419.mp3',
+		},{
+			id: '2',
+			name: '月光の雲海',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/4a09ac1c230b14911b945449dd0302a1/KGTX/CLTX001/3767cd187b64b8dcf9841915b6f0256c.mp3',
+		},{
+			id: '3',
+			name: 'Summer ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071729/fd150154ea349ee2950438c9f2a183a4/KGTX/CLTX001/654ca7d7d4abab8a85cd23523a5e693d.mp3',
+		},{
+			id: '4',
+			name: 'あの夏へ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/60a15126d200ceab629ebf1dcb6adaef/KGTX/CLTX001/100e772a8a7a988993f228332a43a285.mp3',
+		}, {
+			id: '5',
+			name: '千百度',
+			singer: '许嵩',
+			src: 'https://sharefs.ali.kugou.com/202201071705/43a469e37a93e6959da8c3d536058b2c/KGTX/CLTX001/fe75c71646819dc6508922dee8eb8ad6.mp3',
+		}, {
+			id: '6',
+			name: '成都',
+			singer: '赵磊',
+			src: 'https://sharefs.ali.kugou.com/202201071709/9c0265f2869edcdfefff731026ffcd85/KGTX/CLTX001/a06b033b356bfc974c5245d0195086a5.mp3'
+		}]
+	},	{
+		moodType: '恐惧',
+		musicList: [{
+			id: '1',
+			name: '恐惧',
+			singer: '贝多芬',
+			src: 'http://music.163.com/song/media/outer/url?id=1387574419.mp3',
+		},{
+			id: '2',
+			name: '月光の雲海',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/4a09ac1c230b14911b945449dd0302a1/KGTX/CLTX001/3767cd187b64b8dcf9841915b6f0256c.mp3',
+		},{
+			id: '3',
+			name: 'Summer ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071729/fd150154ea349ee2950438c9f2a183a4/KGTX/CLTX001/654ca7d7d4abab8a85cd23523a5e693d.mp3',
+		},{
+			id: '4',
+			name: 'あの夏へ',
+			singer: '久石譲',
+			src: 'https://sharefs.ali.kugou.com/202201071727/60a15126d200ceab629ebf1dcb6adaef/KGTX/CLTX001/100e772a8a7a988993f228332a43a285.mp3',
+		}, {
+			id: '5',
+			name: '千百度',
+			singer: '许嵩',
+			src: 'https://sharefs.ali.kugou.com/202201071705/43a469e37a93e6959da8c3d536058b2c/KGTX/CLTX001/fe75c71646819dc6508922dee8eb8ad6.mp3',
+		}, {
+			id: '6',
+			name: '成都',
+			singer: '赵磊',
+			src: 'https://sharefs.ali.kugou.com/202201071709/9c0265f2869edcdfefff731026ffcd85/KGTX/CLTX001/a06b033b356bfc974c5245d0195086a5.mp3'
+		}]
+	},
+]
