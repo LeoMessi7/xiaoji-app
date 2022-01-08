50c0d430466df0b22175cc2e86afcc7cf14334ba
LeoMessi7 <1367027689@qq.com>
2022-01-09 03:23:54 +0800

    音乐修改

--- pages/equip/function/music.vue
+++ pages/equip/function/music.vue
@@ -1,6 +1,6 @@
 <template>
 	<view style="background-color: #FFFFFF;height: 100%;">
-		<div class="box">
+		<div class="box" >
 			<image :class="this.key?'tag':'tag1'" src="../../../static/music/player_bar.png" mode:widthFix><br>
 			</image>
 			<image src="../../../static/music/disc.png"
@@ -10,9 +10,11 @@
 				<text style="font-weight: 600;font-size: 50rpx;margin-top: 100rpx;">{{name }}<br><span
 						style="font-weight: 500;font-size: 35rpx;">{{singer}}</span></text>
 			</div>
+			
 		</div>
-		<view style="width: 100%;display: flex;margin-top: 10%;">
-			<u-icon name="list" color="#535353" size="30" style="float: right;margin-left: 82%;" @click="open()">
+		<view style="width: 80%;height: 100rpx;margin: auto;">
+			<text style="float: left;font-weight: 600;color:#585858;margin-top: 80rpx;">当前心情：{{moodType}}</text>
+			<u-icon name="list" color="#535353" size="30" style="float: right;margin-top: 80rpx;" @click="open()">
 			</u-icon>
 		</view>
 		<div class="boxa1">
@@ -20,9 +22,9 @@
 				<u-popup :show="isOpen" mode="bottom" @close="close" @open="open()" :closeOnClickOverlay="true">
 					<view>
 						<ul>
-							<li v-for="item in musicList" @click="changeAudio(item)" style="height: 80rpx;font-size: 40rpx;margin-top: 20rpx;">
-								<label>{{item.name}}  {{item.singer}}</label>
-								
+							<li v-for="item in musicList" @click="changeAudio(item)"
+								style="height: 80rpx;font-size: 40rpx;margin-top: 20rpx;">
+								<label>{{item.name}} {{item.singer}}</label>
 							</li>
 						</ul>
 					</view>
@@ -37,7 +39,8 @@
 			<text class="end-time">{{endTime}}</text>
 		</view>
 		<div style="display: flex;text-align: center;margin-left: 28%;padding-bottom: 100rpx;padding-top: 20rpx;">
-			<u-icon name="skip-back-left" color="#535353" size="45" style="float: left;margin-top: 20rpx;" @click="getLastSong()"></u-icon>
+			<u-icon name="skip-back-left" color="#535353" size="45" style="float: left;margin-top: 20rpx;"
+				@click="getLastSong()"></u-icon>
 			<u-icon v-if="this.key===true" name="play-circle" color="#535353" size="60"
 				style="float:left;margin-left: 20rpx;margin-right: 20rpx;" @click="changeState()"></u-icon>
 			<u-icon v-if="this.key===false" name="pause-circle" color="#535353" size="60"
@@ -51,7 +54,9 @@
 </template>
 
 <script>
-	import {recommendList} from "../../../utils/mood.js"
+	import {
+		recommendList
+	} from "../../../utils/mood.js"
 	var innerAudioContext = uni.createInnerAudioContext();
 	innerAudioContext.autoplay = true;
 	export default {
@@ -69,11 +74,11 @@
 			}
 		},
 		onLoad() {
-			let moodNum = Math.round(Math.random()*6);
+			let moodNum = Math.round(Math.random() * 6);
 			console.log(moodNum)
 			uni.getStorage({
 				key: "moodNum",
-				success(res){
+				success(res) {
 					console.log(res.data)
 					moodNum = res.data
 				}
@@ -82,6 +87,11 @@
 			this.moodType = recommendList[moodNum].moodType
 			this.musicList = recommendList[moodNum].musicList
 			console.log(this.moodType)
+			this.name = this.musicList[0].name;
+			this.singer = this.musicList[0].singer;
+			this.id = this.musicList[0].id;
+			innerAudioContext.src = this.musicList[0].src
+			this.endTime = innerAudioContext.duration
 		},
 		methods: {
 			changeState() {
@@ -93,23 +103,24 @@
 				}
 			},
 			changeAudio(e) {
-				this.isOpen=false
+				this.isOpen = false
 				this.key = false;
 				this.name = e.name;
 				this.singer = e.singer;
 				this.id = e.id;
 				innerAudioContext.src = e.src
+				console.log(innerAudioContext._src)
 				this.endTime = innerAudioContext.duration
 				innerAudioContext.onPlay(() => {
 					console.log('1')
 				})
-				
+
 			},
 			getLastSong() {
 				console.log('1')
 				if (this.id <= 1)
 					return
-				var e = this.musicList[this.id-2]
+				var e = this.musicList[this.id - 2]
 				this.key = false;
 				this.name = e.name;
 				this.singer = e.singer;
@@ -117,7 +128,7 @@
 				innerAudioContext.src = e.src
 				this.endTime = innerAudioContext.duration
 				innerAudioContext.onPlay(() => {
-					
+
 				})
 			},
 			getNextSong() {
@@ -159,8 +170,6 @@
 	.box {
 		position: relative;
 		width: 100%;
-
-		background-color: #fff;
 		text-align: center;
 	}
 
@@ -240,115 +249,3 @@
 		}
 	}
 </style>
-<!-- 
-<style>
-/* 	.play-container{
-		position: relative;
-		/*  #ifdef  APP-PLUS*/
-		    box-sizing: border-box;
-		   margin-top: -56rpx;
-		/*  #endif  */
-		width: 100vw;
-		height: 100vh;
-		::v-deep.myuni-topbar{
-			background-color: unset;
-		}
-		.top-info{
-			height: 100rpx
-			::v-deep.left-icon{
-				line-height: 100rpx;
-			}
-			::v-deep.myuni-topbar{
-				line-height: 50rpx;
-			}
-			::v-deep.right-icon{
-			  width: 160rpx;
-			}
-			.song-name-box{
-				display: flex;
-				flex-direction: column;
-				align-items: center;
-				line-height: unset !important;
-			}
-			.right-info{
-				height: 100%;
-				image{
-					box-sizing: border-box;
-					padding: 20rpx;
-					width: 100rpx;
-					height: 100rpx !important;
-					vertical-align: middle;
-				}
-				.uni-icons{
-					margin-left: 5px;
-				}
-			}
-		}
-		.disc-box{
-			position: relative;
-			margin: 200rpx auto;
-			/*  #ifdef MP-WEIXIN */
-			margin: 50px auto;
-			/*  #endif  */
-			width: 510rpx;
-			height: 420rpx;
-			background-color:red;
-			image{
-				width: 100%;
-			}
-		}
-		.rotate-stop{
-			transform: rotate(-45deg) !important;
-		}
-		.control-container{
-			box-sizing: border-box;
-			padding: 20rpx;
-			height: 400rpx;
-			// background-color: red;
-			.first-row-control{
-				display: flex;
-				justify-content: space-around;
-				.control-item{
-					flex: 1;
-					text-align: center;
-					font-size: 40rpx;
-				}
-			}
-			.second-row-control{
-				margin:80rpx 0;
-				display: flex;
-				align-items: center;
-				.myline{
-					flex: 1;
-				}
-				.start-time,
-				.end-time{
-					width: 176rpx;
-					text-align: center;
-				}
-			}
-			.thrid-row-control{
-				display: flex;
-				align-items: center;
-				.control-item{
-					text-align: center;
-					font-size: 40rpx;
-				}
-				.playway{
-					width: 176rpx;
-				}
-				.play-control-box{
-					flex: 1;
-					padding-left: 60rpx;
-					align-items: center;
-					// text-align: center;
-					.play{
-						margin: 0 40rpx;
-						font-size: 80rpx;
-					}
-				}
-			}
-		}
-	} */
-</style>
- -->
